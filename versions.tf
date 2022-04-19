6d2beff850b880a5abf75b810453f9d47bc31565
Damien Laureaux <d.laureaux@timoa.com>
2022-04-19 16:11:43 +0200

    feat(terraform): add Terraform Cloud as backend

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = "~> 1.0"
 
+  backend "remote" {
+    organization = "timoa"
+    workspaces {
+      name = "terraform-github-public"
+    }
+  }
+
   required_providers {
     github = {
       source  = "integrations/github"
