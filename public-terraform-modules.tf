ca46f93d0ee4661712e32e3ce6a1aa5ad6edc2fa
Damien Laureaux <d.laureaux@timoa.com>
2022-04-22 23:41:17 +0200

    feat(oci): add the Oracle Cloud Infrastructure Terraform Module example

--- public-terraform-modules.tf
+++ public-terraform-modules.tf
@@ -63,3 +63,26 @@ module "terraform_module_gcp_example" {
     "managed-by-terraform"
   ]
 }
+
+# Terraform Module OCI Example
+module "terraform_module_oci_example" {
+  source  = "mineiros-io/repository/github"
+  version = "0.16.0"
+
+  name                 = "terraform-module-oci-example"
+  description          = "A Terraform module uses as an example to start new Oracle Cloud Infrastructure Terraform module. It includes all the automation (versioning, providers update, lint, security) and best practices from HashiCorp"
+  default_branch       = "main"
+  gitignore_template   = "Terraform"
+  license_template     = "apache-2.0"
+  defaults             = var.defaults
+  vulnerability_alerts = true
+  topics = [
+    "terraform",
+    "semantic-release",
+    "renovate",
+    "terraform-module",
+    "oracle-cloud-infrastructure",
+    "oci",
+    "managed-by-terraform"
+  ]
+}
