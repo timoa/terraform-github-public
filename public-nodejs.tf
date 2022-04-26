8e6b370ddc35d5ff0dcaa8d23a6a23bd05504863
Damien Laureaux <d.laureaux@timoa.com>
2022-04-26 23:03:28 +0200

    refactor(nodejs): use the local module instead of multiple remote module calls

--- public-nodejs.tf
+++ public-nodejs.tf
@@ -1,14 +1,11 @@
 # App Stores Prometheus Exporter
 module "app_stores_prometheus_exporter" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "app-stores-prometheus-exporter"
-  description          = "Prometheus exporter that exports metrics (score, ratings, reviews, app version, etc.) from Apple App Store and Google Play"
-  default_branch       = "develop"
-  gitignore_template   = "NodeJS"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name               = "app-stores-prometheus-exporter"
+  description        = "Prometheus exporter that exports metrics (score, ratings, reviews, app version, etc.) from Apple App Store and Google Play"
+  default_branch     = "develop"
+  gitignore_template = "NodeJS"
   topics = [
     "nodejs",
     "docker",
@@ -22,15 +19,12 @@ module "app_stores_prometheus_exporter" {
 
 # Chrome Tab Rotate Server
 module "chrome_tab_rotate_server" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "chrome-tab-rotate-server"
-  description          = "A Webserver that provides the config/contents for the chrome-tab-rotate Chrome extension"
-  default_branch       = "develop"
-  gitignore_template   = "NodeJS"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name               = "chrome-tab-rotate-server"
+  description        = "A Webserver that provides the config/contents for the chrome-tab-rotate Chrome extension"
+  default_branch     = "develop"
+  gitignore_template = "NodeJS"
   topics = [
     "nodejs",
     "chrome-extension",
@@ -48,15 +42,12 @@ module "chrome_tab_rotate_server" {
 
 # NodeJS Encryption API Example
 module "nodejs_encryption_api_example" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "nodejs-encryption-api-example"
-  description          = "Example of encrypting/decrypting data thru an API using node.js"
-  default_branch       = "develop"
-  gitignore_template   = "NodeJS"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name               = "nodejs-encryption-api-example"
+  description        = "Example of encrypting/decrypting data thru an API using node.js"
+  default_branch     = "develop"
+  gitignore_template = "NodeJS"
   topics = [
     "nodejs",
     "api",
