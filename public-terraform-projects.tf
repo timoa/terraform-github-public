d11ffd3a46129a606fbecdbaa384258e0a673856
Damien Laureaux <d.laureaux@timoa.com>
2022-04-27 11:53:53 +0200

    refactor(terraform-projects): use the local module instead of multiple remote module calls

--- public-terraform-projects.tf
+++ public-terraform-projects.tf
@@ -1,15 +1,11 @@
 # HomeLab
 module "homelab" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "homelab"
-  description          = "Experimentation of running an HomeLab server running Proxmox + K8S cluster on VMs"
-  default_branch       = "develop"
-  gitignore_template   = "Terraform"
-  license_template     = "apache-2.0"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name             = "homelab"
+  description      = "Experimentation of running an HomeLab server running Proxmox + K8S cluster on VMs"
+  default_branch   = "develop"
+  license_template = "apache-2.0"
   topics = [
     "terraform",
     "proxmox",
@@ -20,16 +16,12 @@ module "homelab" {
 # Terraform Elasticsearch Single Node (OUTDATED)
 # TODO: UPDATE THIS REPOSITORY
 module "terraform_elasticsearch_single_node" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "terraform-elasticsearch-single-node"
-  description          = "Example of the creation of an AWS Elasticsearch single node with Terraform"
-  default_branch       = "master"
-  gitignore_template   = "Terraform"
-  license_template     = "apache-2.0"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name             = "terraform-elasticsearch-single-node"
+  description      = "Example of the creation of an AWS Elasticsearch single node with Terraform"
+  default_branch   = "master"
+  license_template = "apache-2.0"
   topics = [
     "aws",
     "aws-elasticsearch",
@@ -43,16 +35,12 @@ module "terraform_elasticsearch_single_node" {
 # Terraform Jenkins Slaves on-demand (OUTDATED)
 # TODO: UPDATE THIS REPOSITORY
 module "terraform_jenkins_slaves_on_demand" {
-  source  = "mineiros-io/repository/github"
-  version = "0.16.0"
+  source = "./modules/repository"
 
-  name                 = "terraform-jenkins-slaves-on-demand"
-  description          = "Configuration of a Jenkins master with slaves on-demand (AWS EC2 version)"
-  default_branch       = "master"
-  gitignore_template   = "Terraform"
-  license_template     = "apache-2.0"
-  defaults             = var.defaults
-  vulnerability_alerts = true
+  name             = "terraform-jenkins-slaves-on-demand"
+  description      = "Configuration of a Jenkins master with slaves on-demand (AWS EC2 version)"
+  default_branch   = "master"
+  license_template = "apache-2.0"
   topics = [
     "aws",
     "jenkins",
