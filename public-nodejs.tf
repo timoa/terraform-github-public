088ee4c52cb698ea2bab1f1c5f43c9d927dd5233
Damien Laureaux <d.laureaux@timoa.com>
2022-04-30 07:46:38 +0200

    fix(nodejs): fix Node gitignore template name

--- public-nodejs.tf
+++ public-nodejs.tf
@@ -5,7 +5,7 @@ module "app_stores_prometheus_exporter" {
   name               = "app-stores-prometheus-exporter"
   description        = "Prometheus exporter that exports metrics (score, ratings, reviews, app version, etc.) from the Apple App Store and Google Play Store"
   default_branch     = "develop"
-  gitignore_template = "NodeJS"
+  gitignore_template = "Node"
   topics = [
     "nodejs",
     "docker",
@@ -24,7 +24,7 @@ module "chrome_tab_rotate_server" {
   name               = "chrome-tab-rotate-server"
   description        = "A Webserver that provides the config/contents for the chrome-tab-rotate Chrome extension"
   default_branch     = "develop"
-  gitignore_template = "NodeJS"
+  gitignore_template = "Node"
   topics = [
     "nodejs",
     "chrome-extension",
@@ -47,7 +47,7 @@ module "nodejs_encryption_api_example" {
   name               = "nodejs-encryption-api-example"
   description        = "Example of encrypting/decrypting data thru an API using node.js"
   default_branch     = "develop"
-  gitignore_template = "NodeJS"
+  gitignore_template = "Node"
   topics = [
     "nodejs",
     "api",
@@ -67,7 +67,7 @@ module "app_stores_domo_data_collection" {
   name               = "app-stores-domo-data-collection"
   description        = "Continuously import App Stores metrics (score, ratings, reviews, app version, etc.) from the Apple App Store and Google Play Store to a DOMO Dataset"
   default_branch     = "main"
-  gitignore_template = "NodeJS"
+  gitignore_template = "Node"
   topics = [
     "nodejs",
     "docker",
